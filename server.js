168750d9c79eaa3d2e07d016a084b05f4c96132d
Honza Škovran <honza@skovran.net>
2017-06-06 22:14:20 +0200

    add access control headers

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ require('dotenv').load();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes(app);
